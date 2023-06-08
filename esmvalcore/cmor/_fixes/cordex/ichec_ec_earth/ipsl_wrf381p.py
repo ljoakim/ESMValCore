03070696a0d7bc1183408aae5e01fdc4e4db33d6
Joakim Low <joakim.low@smhi.se>
2023-06-08 13:48:38 +0200

    Adding height fix for WRF381P

---
+++ esmvalcore/cmor/_fixes/cordex/ichec_ec_earth/ipsl_wrf381p.py
@@ -0,0 +1,9 @@
+"""Fixes for rcm WRF381P driven by ICHEC-EC-EARTH."""
+from esmvalcore.cmor._fixes.cordex.cordex_fixes import (
+    IPSLWRF381P as BaseFix)
+
+Tas = BaseFix
+
+Tasmax = BaseFix
+
+Tasmin = BaseFix
---
+++ esmvalcore/cmor/_fixes/cordex/ipsl_ipsl_cm5a_mr/ipsl_wrf381p.py
@@ -0,0 +1,9 @@
+"""Fixes for rcm WRF381P driven by IPSL-IPSL-CM5A-MR."""
+from esmvalcore.cmor._fixes.cordex.cordex_fixes import (
+    IPSLWRF381P as BaseFix)
+
+Tas = BaseFix
+
+Tasmax = BaseFix
+
+Tasmin = BaseFix
